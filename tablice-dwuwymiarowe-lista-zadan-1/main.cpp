90f4dbcaf5556957dd1b8e94e4c2a48d6bbb2828
Kuba Wtorkiewicz <kuba.wtorkiewicz@icloud.com>
2021-11-10 20:07:24 +0100

    Initial Commit

---
+++ tablice-dwuwymiarowe-lista-zadan-1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+//zadeklarowanie tablicy ze stałymi wartościami i wypisanie jej
+void zadaniepierwsze()
+{
+    
+}
+
+
+
+
+int main() {
+    
+    
+    
+    
+    return 0;
+}
